c13a14330040d555cb73992a26d9386289344d6a
maykon-jds <maykondias2001@gmail.com>
2024-03-17 17:32:57 -0300

    SAVEPOINT

--- src/config/FileDatabaseConfigurationExtractorINIStrategy.php
+++ src/config/FileDatabaseConfigurationExtractorINIStrategy.php
@@ -6,28 +6,40 @@ use Class\Strategy\FileDatabaseConfigurationExtractorStrategy;
 
 class FileDatabaseConfigurationExtractorINIStrategy extends FileDatabaseConfigurationExtractorStrategy {
 
+    function __construct(string $filename)
+    {
+
+        // $ini_array = parse_ini_file($filename);
+
+    }
+
 
     public function getHost() : String {
 
-        
+        return "";
 
     }
     
     public function getUsername() : String {
 
-        
+        return "";
 
     }
     
     public function getPassword() : String {
 
-        
+        return "";
 
     }
 
     public function getDatabase() : String {
 
-        
+        return "";
+
+    }
+
+    protected function checkIfTheFileExists(String $filename) : void {
+
 
     }
 
